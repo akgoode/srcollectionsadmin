9efa240c74fa9416f1055cd06cd0044e5f9c2829
akgoode <akgoode@gmail.com>
2018-01-14 22:58:25 -0600

    localhost server works

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 
 var app = express();
-
+app.use('/scripts', express.static(__dirname + '/node_modules/'));
 app.use(express.static(__dirname + '/public'));
 
 var port = 3200;
